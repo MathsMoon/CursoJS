878daec4287ae919908c1e085545c9513ff3bbef
Matheus A <matheusalvessouza1@hotmail.com>
2025-01-07 23:57:19 -0300

    update 07_01
    Finaliza tudo logo, temos pouco tempo.

--- JavaScript_basico/aula_JsLogic/exe_date/script.js
+++ JavaScript_basico/aula_JsLogic/exe_date/script.js
@@ -1,26 +1,48 @@
-//Seção das variáveis Globais do escopo:
-const date = new Date(); 
+//Função que carrega após o término de Load do site:
+window.onload = function loading_site() {
+    //Seção das variáveis Globais do escopo:
+    const date = new Date(); 
+    const h1 = document.querySelector('.container h1');
 
+    //Função principal do escopo, reuni todas as informações nela:
+    function main_Function() {
+        //Inserindo a nova data formatada.
+        const new_date = time_Correction();
 
-function main_Function() {
-    //Inserindo a nova data formatada.
-    const new_date = time_Correction();
+        //Inserindo a nova data no H1:
+        over_Writes(new_date);
+    }
 
-    //Inserindo a nova data no H1:
-    over_Writes();
-}
+    //Função que rescreve o H1 para o horário atual:
+    function over_Writes(date){
+        h1.textContent = `${date}`;
+    }
 
-//Função que rescreve o H1 para o horário atual:
-function over_Writes(){
+    //Função que formata a data para o tipo certo
+    function time_Correction() {
+        //Definindo o padrão de visualização do objeto:
+        const options = { 
+            weekday: 'long', 
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric', 
+            hour: 'numeric', 
+            minute: 'numeric'
+        };
+        
+        //definindo e retornando o objeto formatado:
+        const new_date = (date.toLocaleDateString('pt-BR', options));
+        return new_date;
+    }
+
+    //Executando a função principal ao carregar do site:
+    main_Function();
+
+    /* Versão reduzida de tudo acima: 
     
+    h1.innerHTML = DataTransfer.toLocaleDateString('pt-BR', {
+        dateStyle: 'full',
+        timeStyle: 'short'
+    });
+    */
 }
-
-//Função que formata a data para o tipo certo
-function time_Correction() {
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
-    const new_date = (date.toLocaleDateString('pt-BR', options));
-    return new_date;
-}
-
-//Executando a função principal ao carregar do site:
-main_Function();
