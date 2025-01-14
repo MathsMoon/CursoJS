49a2b248654ea2c590ffd869422e9fcc6a1dc496
Matheus A <matheusalvessouza1@hotmail.com>
2025-01-13 23:56:39 -0300

    update 13_01
    O que horas de foco irão se tornar?
    
    Descubra no update 14_01

---
+++ JavaScript_basico/aula_JsLogic/setInterval.js
@@ -0,0 +1,28 @@
+/* Intervalo e TimeOut */
+
+//função que retorna a hora atual:
+function return_HourNow(){
+    let data = new Date(); //criando hora
+    return data.toLocaleTimeString('pt-BR'); //retornando no estilo PT-BR, time zone do brasil.
+}
+
+/* Para que a função do retorno do horário funcione de forma a ser chamada continuamente, o setInterval
+precisa receber na sua chamada uma função que se encarrega de ser a "solicitação de execução", ou seja, 
+uma função que será acionanda pelo setInterval para que a cada vez que ela for confirmada, todas as chamadas
+dentro desta função executem, por hora só temos a função de retorno do horário. E é isso que ela vai retornar.
+
+Usamos então uma função anônima dentro do setInterval para realizar essa chamada, como demosntra abaixo.
+
+Para pausar o retorno do setInterval, já que ele apenas recebe de quanto em quanto tempo acionar, e não tem um limite
+usamos o setTimeOut para ser chamado uma única vez e encerrar o processo do setInterval.
+*/
+
+//Definindo a função anônima de chamada e o intervalo para cada chamada. 1000 -> 1s
+setInterval(function () {
+    console.log(return_HourNow());
+}, 1000);
+
+//Definindo o setTimeOut para parar toda a chamada do setInterval:
+setTimeout(function () {
+    
+}, );
--- JavaScript_basico/aula_JsLogic/try.js
+++ JavaScript_basico/aula_JsLogic/try.js
@@ -8,21 +8,21 @@
 // }
 
 //Utilizando o try em uma situação real:
-function soma(x,y) {
-    if(typeof x !== 'number' || typeof y !== 'number') {
-        throw new Error('x e y precisam ser números.'); //Retornando um erro com esta frase e os detalhes de onde ocorreu.
-    }
+// function soma(x,y) {
+//     if(typeof x !== 'number' || typeof y !== 'number') {
+//         throw new Error('x e y precisam ser números.'); //Retornando um erro com esta frase e os detalhes de onde ocorreu.
+//     }
 
-    return x + y;
-}
+//     return x + y;
+// }
 
-//Mostrando o resultado:
-try {
-    console.log(soma(1,23));
-    console.log(soma(1,"a")); //captura este erro e exibe a mensagem de erro abaixo, tudo abaixo deste erro não será executado.
-}catch (err){
-    console.log(err); //envia a mensagem completa de erro do throw.
-}
+// //Mostrando o resultado:
+// try {
+//     console.log(soma(1,23));
+//     console.log(soma(1,"a")); //captura este erro e exibe a mensagem de erro abaixo, tudo abaixo deste erro não será executado.
+// }catch (err){
+//     console.log(err); //envia a mensagem completa de erro do throw.
+// }
 
 /* Usando o Try com o bloco finally */
 
@@ -32,10 +32,25 @@ function return_TimeNow(data) {
         throw new TypeError('Esperando tipo Date'); //retornando o tipo esperando através de um TypeError.
     }
 
+    //Caso o valor do date não seja uma instância da classe, aqui ele recebe a instância:
     if(!data){ 
         data = new Date();
     }
+
+    //retornando o valor do tempo com padrão do Brasil:
+    return data.toLocaleTimeString('pt-BR', {
+        // hour12: true -> Por padrão ele retorna no formato 24 horas, mas fazendo isso, se utiliza o AM e PM.
+    });
 }
 
+/* Testando possíveis erros:
+const hour = return_TimeNow(11); -> Erro por não inserir uma instância do tipo Date.
+console.log(hour);
+const data = new Date('01-01-2000 00:00:00'); -> Retorna o horário conforme descrito no data. resultado: 00:00:00
+const hour = return_TimeNow(data);
+console.log(hour);
+*/
+
 //Mostrando o resultado:
-return_TimeNow();
+const hour = return_TimeNow(); //Quando não se insere um valor via date, ele retorna o valor atual do horário.
+console.log(hour);
