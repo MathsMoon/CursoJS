da554062241488a28382b1705863bad61242cb77
Matheus A <matheusalvessouza1@hotmail.com>
2025-01-22 23:52:31 -0300

    update 22_01

---
+++ Exercicios/js/coderDesafios.js
@@ -0,0 +1,45 @@
+/* Desafios do curso de Funções da CODER */
+
+//Desafio #1 - Função de Range:
+
+function range(num, max, btw_num = 1) {
+    //Definição das variáveis
+    const n1 = max == undefined ? 1: num; //Caso o max seja vazio recebe 1 ou o próprio número.
+    const n2 = max == undefined ? num: max; 
+    const step = n1 <= n2 ? Math.abs(btw_num) : - Math.abs(btw_num); //Ele inverte a lógica do For para caso o valor seja de incrementação ou não
+    const nums = [];
+    
+    //Lógica do For para que se comporte de acordo com o max e o num
+    for(let i = n1; n1 <= n2 ? i <= n2 : i >= n2; i += step) {
+        nums.push(i);
+    }
+    
+    //retornando o array com os resultados
+    return nums;
+}
+
+//Mostrando o resultado:
+// console.log(range(5));
+// console.log(range(6, 11));
+// console.log(range(10,19, 2));
+// console.log(range(6, 2));
+// console.log(range(8, -3, 4));
+
+//Desafio #2 - Manipulando as informações de um Carrinho.
+
+/* Utilizando o Filter, Map e Reduce. Pegue o objeto abaixo do carrinho e:
+- Filtre e mostre todos que são frágeis. 
+- Depois pegue todos os produtos e ordene por Quantidade * preço para saber a média de cada produto.
+- Com o preço de todos os produtos, cálcule a média Total de todos os produtos */
+
+const shopList = [
+    { name: 'Caneta', amount: 10, price: 7.99, fragile: true},
+    { name: 'Impressora', amount: 1, price: 649.50, fragile: true},
+    { name: 'Caderno', amount: 4, price: 27.10, fragile: false},
+    { name: 'Lapis', amount: 3, price: 5.82, fragile: false},
+    { name: 'Tesoura', amount: 1, price: 19.20, fragile: true}
+];
+
+function returning_Organized_ShopList(){
+
+}
--- JavaScript_basico/aula_JsFunction/function.js
+++ JavaScript_basico/aula_JsFunction/function.js
@@ -1,18 +1,25 @@
 /* Funções em geral no Javascript 
 
-    Existem diferentes tipos de declaração de função, cada uma delas com um propósito que torna melhor a sua implementação.
-    O primeiro e tipo mais comum é a função declarada de forma direta:
-    Function alguma_Coisa() {} -> esta declaração mais direta leva consigo o modelo padrão de declaração, como também faz uso do
-    Hoisting, que é a elevação de prioridade ao escopo global, desta forma independente da sua posição dentro do arquivo, o seu 
-    uso será assegurado e garantido devido ao alto nível de prioridade que é dada através do Hoisting.
     As funções são objetos de primeira classe, tanto possuem a capacidade de receber dados e transforma-los, como elas próprias
     podem ser esses dados a serem guardados em uma variável. Ao serem inseridas como dado em uma variável, a mesma se torna a função
     e carrega tudo que é descrito dentro da função, portanto é possível se utilizar uma variável como parâmetro de outra função, tendo
     em seu conteúdo uma outra função.
 
-    Dentre os outros tipos de funções, temos a Arrow function. Uma versão reduzida do modelo tradicional que tem como propriedade já
-    a naturalidade de ser criada para uma variável ou parametro, seu comportamento em relação ao This se difere das demais por não 
-    se utilizar da palavra reservada function.
+    Existem diferentes tipos de declaração de função, cada uma delas com um propósito que torna melhor a sua implementação.
+    O primeiro e tipo mais comum é a função declarada de forma direta:
+    
+    Function alguma_Coisa() {} -> esta declaração mais direta leva consigo o modelo padrão de declaração, como também faz uso do
+    Hoisting, que é a elevação de prioridade ao escopo global, desta forma independente da sua posição dentro do arquivo, o seu 
+    uso será assegurado e garantido devido ao alto nível de prioridade que é dada através do Hoisting.
+
+    A outra forma mais comum é a função anônima como exemplo: 
+    function () {} -> Esta função não tem nome, sendo muito utilizada para servir como escopo de alguma funcionalidade que precisamos
+    sem a necessidade de criar uma função tradicional para ela, utilizamos apenas a sua estrutura para retornar ou realizar uma atividade.
+    É usada muito em conjunto com as functions expressions.
+
+    Dentre os outros tipos de funções, temos a Arrow function. Exemplo:
+    const sum_arrow = (a, b) => {} -> Uma versão reduzida do modelo tradicional que tem como propriedade já a naturalidade de ser criada para
+    uma variável ou parametro, seu comportamento em relação ao This se difere das demais por não se utilizar da palavra reservada function.
     
     Esta mesma palavrada dá as funções expressadas com ela algumas outras possibilidades que a arrow function não possue, um exemplo disso
     é a variável arguments, que recebe tudo que foi passado para a execução da função, um array que guarda tudo que foi descrito, sendo
@@ -20,9 +27,16 @@
     Como resposta a chamada dessas variáveis ele pode retornar undefined, já que os parâmetros foram declarados mas não atribuídos a valor 
     algum.
 
+    Um ponto importante para ser considerado das funções anônimas é o IIFE, como estas não podem ser chamadas, já que não são nomeadas, 
+    elas possuem uma forma única de chamada que designa os valores dos parâmetros logo após a criação da mesma. Portanto as funções que não
+    são atreladas a alguma outra atividade, podem ser chamadas como no exemplo abaixo:
+
+    (function (a,b) {
+        return console.log(a + b);
+    })(1,2);
+
     Outra possibilidade em relação aos parâmetros é a utilização do parametro rest, que lida com todas as informações posteriores as aquelas
     definidas por padrão, guardando elas de forma agrupada em uma variável.
-
 */
 
 //Exemplo de como a função não é presa ao escopo ou posição global:
