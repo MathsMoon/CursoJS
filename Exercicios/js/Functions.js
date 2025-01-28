9c02676997dac0cc7457b4922a6a10417a270ab5
Matheus A <matheusalvessouza1@hotmail.com>
2025-01-27 23:56:48 -0300

    update 27_01
    Eu tenho que continuar estudando logo

--- Exercicios/js/Functions.js
+++ Exercicios/js/Functions.js
@@ -1,12 +1,42 @@
 /* Atividades gerais de Funções */
 
 // 113 - A partir de um array como ['maçã', 'banana', 'uva'], a função deve retornar: "1. maçã, 2. banana, 3. uva".
+function mapFruit(...fruit){
+    /* 
+        O map serve para percorrer o array e permite dentro dele a criação
+        de funções, nesta criamos uma arrow function com item e index, separando
+        as duas informações principais: os itens do array e seu index.
+        Depois só concatenar as informações com template Strings
+    */
+    return fruit.map((item, index) => `${index + 1}. ${item}`).join(', ');
+}
+
+//Mostrando o resultado:
+let fruits = mapFruit('Banana', 'Maçã', 'Uva');
+//console.log(fruits);
 
 // 118 - Retorne a soma de todas os valores dentro de um Arrow com array function:
+const array_sum = (...sum) => sum.reduce((acc, curr) => acc + curr, 0);
+//console.log(array_sum(1,2,3,4,5)); 
 
 // 124 - Cópia profunda de Objetos:
+const generic = {
+    statement1: 'value1',
+    statement2: 'value2',
+    statement3: 'value3',
+}
+
+function copyingObj(obj) {
+    return {...obj};
+}
+
+//Mostrando o resultado:
+//console.log(copyingObj(generic)); //Objeto copiado e completamente diferente do generic padrão.
+generic.statement1 = 'value4';
+//console.log(generic);
 
 // 127 - Mesclando dois objetos para se tornarem 1 só:
+
 
 /*  131 - Implemente uma função que aceite dois arrays. O primeiro
 array é uma lista de objetos com uma chave e valor, e o segundo é uma
@@ -19,10 +49,28 @@ objetos e a importância de iterar corretamente sobre eles para alcançar o resu
 desejado.
 */
 
-// 132 - Desestruturando um Objeto:
+
+// 132 - Desestruturando um Objeto e criando outro
+const extracting_Obj = { 
+    state1:generic.statement1,
+    state2: generic.statement2
+}
+
+//console.log(extracting_Obj); //recebe certos statements do generic.
+
+// 132.2 - Função que cria um objeto e retorna seus valores:
+function extracting_Statements(person){
+    const {name, age} = person;
+    return `Meu nome é: ${name}, tenho ${age} anos de idade`;
+}
+
+const person = {name: 'MAB', age: 20, addres: {street: '123', numb: 1}, ID: 12};
+console.log(extracting_Statements(person));
 
 // 133 - Desestruturando um Array:
 
+
 // 134 - Desestruturando um Objeto Aninhado:
+
 
 // 138 - Desestruturando um Array Aninhado:
--- JavaScript_basico/aula_JsArray/Array_Revision.js
+++ JavaScript_basico/aula_JsArray/Array_Revision.js
@@ -5,13 +5,38 @@ console.log(names);
 
 //Deletando o item do array (não seu espaço): Não altera os índices
 delete names[2];
-console.log(names);
+//console.log(names);
 
 //Copiando um Array, sem que ele se torne uma referência na memória
 let nome = [...names];
 
 nome.pop();
-console.log(nome);
-console.log(names);
+//console.log(nome);
+// console.log(names);
+
+//Usando o slice:
+let new_array = nome.slice(0,-1); //Retirando o da ponta direita com o -1
+//console.log(new_array); 
+
+//Convertendo uma String em um Array:
+let nobleTitles = 'Pela Graça de Deus, Imperador da Áustria, Rei da Hungria, Rei da Boémia, de Dalmácia, Croácia, Eslovênia, Galícia, Lodoméria e Ilíria; Rei de Jerusalém, etc... Arquiduque da Áustria, Grão-Duque de Toscana e Cracóvia, Duque de Lorena e Salzburgo, de Estíria, Caríntia, Carníola e Bucovina; Grão-príncipe da Transilvânia; marquês da Morávia; Duque da Alta e da Baixa Silésia, de Módena, Parma e Placência e Guastalla, de Auschwitz, Zator e Teschen, Friul, Ragusa e Zara; conde de Habsburgo e Tirol, de Ciburgo, Gorízia e Gradisca; Príncipe de Trento e Brixen; marquês da Alta e da Baixa Lusácia e da Ístria; Conde de Hohenems, Feldkirch, Bregenz, Sonnenberg, etc...; Senhor de Trieste, de Cattaro, e de Marca Wendia; Grão Voivoda da Sérvia.';
+let arrayTitles = nobleTitles.split(',' || ';');
+//console.log(arrayTitles);
+
+//Convertendo o Array para uma String:
+let newString = arrayTitles.join(',');
+//console.log(newString);
 
 // Invertendo um Array de Nomes:
+const inv_names = names.reverse();
+//console.log(inv_names);
+
+/* Método Splice */
+
+//Removendo o item vazio do array de nomes com método splice:
+let removeds = names.splice(1, 1); //Removendo o <1 empty item>
+//console.log(names, removeds);
+
+// Adicionando elementos ao Array via splice:
+names.splice(names.length, 0, 'Luis');
+console.log(names);
